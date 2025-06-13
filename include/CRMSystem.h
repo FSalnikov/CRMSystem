eebfdc2dd0432ec18dea58a761e41efa842a7429
DrAlt0Clef <ivangorobchenko2006@gmail.com>
2025-06-13 16:59:40 -0400

    CRMSystem.h+

--- include/CRMSystem.h
+++ include/CRMSystem.h
@@ -1,45 +1,4 @@
 #pragma once
-
-#include "Client.h"
-#include <vector>
-#include <optional>
-#include <string>
-#include <nlohmann/json.hpp>
-
-/// @brief Основной класс CRM системы
-class CRMSystem {
-public:
-    /// @brief Добавить клиента
-    void addClient(const Client& client);
-
-    /// @brief Удалить клиента по имени
-    bool removeClient(const std::string& name);
-
-    /// @brief Найти клиента по имени, телефону или email
-    std::optional<Client*> findClient(const std::string& query);
-
-    /// @brief Получить всех клиентов, отсортированных по имени
-    std::vector<Client*> getAllClientsSorted() const;
-
-    /// @brief Сохранить клиентов в файл JSON
-    void saveToFile(const std::string& filename) const;
-
-    /// @brief Загрузить клиентов из JSON
-    void loadFromFile(const std::string& filename);
-
-    /// @brief Получить количество клиентов в каждой категории
-    std::map<ClientCategory, int> getClientCategoryStats() const;
-
-    /// @brief Получить общее количество клиентов
-    size_t getTotalClients() const;
-
-    /// @brief Получить все актуальные задачи (не выполненные)
-    std::vector<Task> getAllPendingTasks() const;
-
-private:
-    std::vector<Client> clients;
-};
-#pragma once
 #include <vector>
 #include <string>
 #include <optional>
@@ -56,23 +15,23 @@ private:
 public:
     /**
      * @brief Добавить клиента
-     * @param client Клиент
+     * @param client Клиент для добавления
      */
     void addClient(const Client& client);
 
     /**
-     * @brief Найти клиента по имени, email или телефону
-     * @param query Поисковый запрос
-     * @return Указатель на найденного клиента (или nullopt)
-     */
-    std::optional<Client*> findClient(const std::string& query);
-
-    /**
      * @brief Удалить клиента по имени
-     * @param name Имя клиента
-     * @return true, если клиент был удалён
+     * @param name Имя клиента для удаления
+     * @return true если клиент был найден и удалён, false если не найден
      */
     bool removeClient(const std::string& name);
+
+    /**
+     * @brief Найти клиента по имени, телефону или email
+     * @param query Поисковый запрос
+     * @return optional с указателем на клиента, если найден
+     */
+    std::optional<Client*> findClient(const std::string& query);
 
     /**
      * @brief Получить всех клиентов, отсортированных по имени
@@ -81,31 +40,32 @@ public:
     std::vector<Client*> getAllClientsSorted();
 
     /**
-     * @brief Получить все невыполненные задачи
-     * @return Список задач
-     */
-    std::vector<Task> getAllPendingTasks() const;
-
-    /**
-     * @brief Получить статистику по категориям
-     * @return Карта <Категория, Количество>
-     */
-    std::map<ClientCategory, int> getClientCategoryStats() const;
-
-    /**
-     * @brief Сохранить данные в файл
-     * @param filename Имя файла
+     * @brief Сохранить клиентов в файл JSON
+     * @param filename Имя файла для сохранения
      */
     void saveToFile(const std::string& filename) const;
 
     /**
-     * @brief Загрузить данные из файла
-     * @param filename Имя файла
+     * @brief Загрузить клиентов из JSON
+     * @param filename Имя файла для загрузки
      */
     void loadFromFile(const std::string& filename);
 
     /**
-     * @brief Получить общее число клиентов
+     * @brief Получить количество клиентов в каждой категории
+     * @return Карта с количеством клиентов по категориям
+     */
+    std::map<ClientCategory, int> getClientCategoryStats() const;
+
+    /**
+     * @brief Получить общее количество клиентов
+     * @return Количество клиентов в системе
      */
     size_t getTotalClients() const;
+
+    /**
+     * @brief Получить все актуальные задачи (не выполненные)
+     * @return Вектор невыполненных задач
+     */
+    std::vector<Task> getAllPendingTasks() const;
 };
